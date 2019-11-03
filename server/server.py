dcd1bbe58d31c9ed179002c1bd901dca064f51e0
yasty <mf3040@nyu.edu>
2019-11-03 11:31:51 -0800

    player auth working

--- server/server.py
+++ server/server.py
@@ -51,10 +51,10 @@ else:
 app.config['ADMIN_TOKEN'] = config.pop('admin_token').lower()
 app.config['PLAYER_TOKENS'] = {}
 if 'players' in config:
-    for entry in config['players']:
+    for i, entry in enumerate(config['players']):
         player_name = entry['name']
         player_token = entry['token']
-        app.config['PLAYER_TOKENS'][player_token] = player_name
+        app.config['PLAYER_TOKENS'][player_token] = {'name': player_name, 'id': i}
 
 def admin_authorize(f):
     @wraps(f)
@@ -79,8 +79,8 @@ def player_authorize(f):
         token = data.lower().replace('bearer: ', '')
         if token not in app.config['PLAYER_TOKENS']:
             abort(401)
-        player_name = app.config['PLAYER_TOKENS'][token]
-        return f(player_name, *args, **kwargs)
+        player = app.config['PLAYER_TOKENS'][token]
+        return f(player, *args, **kwargs)
     return decorated_function
     
 e = engine.Engine(socketio=socketio, **config)
@@ -101,26 +101,30 @@ def get_state():
     return jsonify(list(e.mars.core.bytes))
 
 @app.route('/stage', methods=['POST'])
-def stage_program():
+@player_authorize
+def stage_program(player):
     """
     POST /stage
     Expects a POST with JSON containing a player ID
     and the yeetcode assembly instructions to stage for that player
     Example:
-    $ curl -H 'content-type: application/json' -d '{"player_id": 0, "instructions": "YEET #0, #4"}' 
-    -XPOST localhost:5000/stage
+    $ curl \
+        -H 'content-type: application/json' \
+        -H 'Authorization: Bearer: token1' \
+        -d '{"instructions": "YEET #0, #4"}' \
+        -XPOST localhost:5000/stage
     {"status":"success"} 
     """
-    if 'player_id' not in request.json:
-        return jsonify({'error': 'player_id arg required'})
+    if not request.json:
+        return jsonify({'error': 'no content'})
     if 'instructions' not in request.json:
         return jsonify({'error': 'instructions arg required'})
 
-    player_id = str(request.json['player_id'])
+    player_id = player['id']
     instructions = str(request.json['instructions']).split('\n')
     if len(instructions) > e.max_staging_size:
         instructions = instructions[:e.max_staging_size]
-    e.staged_payloads[int(player_id)] = [int(player_id), instructions]
+    e.staged_payloads[player_id] = [player_id, instructions]
     return jsonify({'status': 'success'})
   
 @socketio.on('connect')
